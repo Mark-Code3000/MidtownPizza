18b7d6063753e022c69f6fb7166a88075178373b
Markeith Eley <meleynyc@gmail.com>
2025-11-13 12:55:18 -0500

    add classes

---
+++ src/main/java/com/pluralsight/models/Pizza.java
@@ -0,0 +1,20 @@
+package com.pluralsight.models;
+//pizza properties here
+//size 8, 14, 16
+//base price
+//crust thin, regular, pan, cauliflower
+//sauce marinara, pesto, bbq, buffalo
+//meat pep, sausage, ham, meatball, bacon, chicken
+//cheese mozz, parm, ricotta,
+
+
+//pizza constructor
+public class Pizza {
+    private int size;
+    private double basePrice;
+    private String crust;
+    private String sauce;
+    private String meat;
+    private String cheese;
+    private String toppings;
+}
